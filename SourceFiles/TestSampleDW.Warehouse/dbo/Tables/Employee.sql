4d9b4cc37d4611b4d5d7c6f230ac52746f426946
BalaFabric <Bala@vgad222.onmicrosoft.com>
2026-05-26 13:14:49 +0530

    New Changes

---
+++ SourceFiles/TestSampleDW.Warehouse/dbo/Tables/Employee.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[Employee] (
+
+	[EmployeeID] int NULL, 
+	[EmployeeName] varchar(100) NULL, 
+	[Department] varchar(100) NULL, 
+	[Salary] decimal(10,2) NULL, 
+	[JoiningDate] date NULL
+);
